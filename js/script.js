64a4c996d466670cdac6542739616b36e827825a
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-10-30 09:42:42 -0700

    Navbar & Header sections finished.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// Add custom JavaScript here
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+  
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 35) {
+        navbar.classList.add('bg-light');
+      } else {
+        navbar.classList.remove('bg-light');
+      }
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', userScroll);
